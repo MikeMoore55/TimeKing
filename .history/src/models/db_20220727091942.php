4a70bd44148d7ca71da256811e7b59531b2487d1
Michael Moore <michaelwillemmoore@gmail.com>
2022-07-27 09:39:44 +0200

    db config file set up
    created and set up database and database config file

---
+++ .history/src/models/db_20220727091942.php
@@ -0,0 +1,23 @@
+<?php
+namespace App\Models;
+/* config file for database */
+use \PDO;
+
+class DB
+{
+    private $host = 'localhost';
+    private $user = 'root';
+    private $pass = 'root';
+    private $dbname = 'timekings_db';
+
+    public function connect()
+    {
+        $conn_str = "mysql:host=$this->host;dbname=$this->dbname";
+        $conn = new PDO($conn_str, $this->user, $this->pass);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        return $conn;
+    }
+}
+
+?>
