d7b3b986c9a963bef2023733ef8272d1bc1bffe1
Michael Moore <michaelwillemmoore@gmail.com>
2022-07-28 11:59:37 +0200

    scroll btns functionality done
    scroll btns can now nav to top or bottom

---
+++ .history/src/js/home_20220728115815.js
@@ -0,0 +1,9 @@
+function topPage() { // Top of page
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+};
+
+function bottomPage() { // Bottom of page
+    document.body.scrollTop = 100000;
+    document.documentElement.scrollTop = 100000;
+};
