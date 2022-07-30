439a271a2b4b936c38a1b78d2384c91502b02d2e
Michael Moore <michaelwillemmoore@gmail.com>
2022-07-30 08:59:25 +0200

    created a user class
    created a user class to create a signed in user object to use when user signs in

---
+++ .history/src/models/user_20220730084222.php
@@ -0,0 +1,7 @@
+<?php
+// require auto load
+require __DIR__ . '/../../vendor/autoload.php';
+
+// require db connection file
+require __DIR__ . '/db.php'; 
+?>
---
+++ .history/src/models/user_20220730085239.php
@@ -0,0 +1,31 @@
+<?php
+
+class SignedInUser implements JsonSerializable{
+    public $userEmail;
+    public $userPassword;
+    public $userDisplayName;
+    public $userCart;
+
+    public function __construct($userEmail, $userPassword, $userDisplayName, $userCart)
+    {
+        $this-> userEmail = $userEmail;
+        $this-> userPassword = $userPassword;
+        $this-> userDisplayName = $userDisplayName;
+        $this-> userCart = $userCart;
+    }
+
+    public function jsonSerialize() {
+    
+        $assocArray = [
+            "userEmail" => $this-> userEmail,
+            "userPassword" => $this-> userPassword,
+            "userDisplayName" => $this-> userDisplayName,
+            "userCart" => $this-> userCart
+        ];
+
+        return $assocArray;
+    }
+}
+
+
+?>
