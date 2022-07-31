4a1e0edcca2fe37ba6d9805251e4942767ed30e8
Michael Moore <michaelwillemmoore@gmail.com>
2022-07-31 16:49:09 +0200

    user info can now be displayed
    user info can be displayed

---
+++ .history/src/js/account_20220731164655.js
@@ -0,0 +1,48 @@
+/* get selected product */
+import signedInUser from "../php/signedInUser.json" assert { type: "json" };
+
+const { createApp } = window.Vue;
+const component = {
+    data(){
+        return{
+            user : signedInUser
+        }
+    },
+    methods: {
+        displayEmail(){
+            document.querySelector("#email").style.display = "block";
+        },
+        
+        displayCart(){
+            document.querySelector("#cart").style.display = "block";
+        },
+        
+        displayPassword(){
+            document.querySelector("#password").style.display = "block";
+        },
+
+        hideEmail(){
+            document.querySelector("#email").style.display = "block";
+        },
+        
+        hideCart(){
+            document.querySelector("#cart").style.display = "block";
+        },
+        
+        hidePassword(){
+            document.querySelector("#password").style.display = "block";
+        }
+    },
+    mounted() {
+        console.log(this.user)
+        this.displayEmail(),
+        this.displayCart(),
+        this.displayPassword()
+
+    },
+}
+/* mount on main */
+window.addEventListener('DOMContentLoaded', () => {
+  const app = createApp(component)
+  app.mount("#user-info")
+})
---
+++ .history/src/js/account_20220731164715.js
@@ -0,0 +1,48 @@
+/* get selected product */
+import signedInUser from "../php/signedInUser.json" assert { type: "json" };
+
+const { createApp } = window.Vue;
+const component = {
+    data(){
+        return{
+            user : signedInUser
+        }
+    },
+    methods: {
+        displayEmail(){
+            document.querySelector("#email").style.display = "block";
+        },
+        
+        displayCart(){
+            document.querySelector("#cart").style.display = "block";
+        },
+        
+        displayPassword(){
+            document.querySelector("#password").style.display = "block";
+        },
+
+        hideEmail(){
+            document.querySelector("#email").style.display = "none";
+        },
+        
+        hideCart(){
+            document.querySelector("#cart").style.display = "none";
+        },
+        
+        hidePassword(){
+            document.querySelector("#password").style.display = "none";
+        }
+    },
+    mounted() {
+        console.log(this.user)
+        this.displayEmail(),
+        this.displayCart(),
+        this.displayPassword()
+
+    },
+}
+/* mount on main */
+window.addEventListener('DOMContentLoaded', () => {
+  const app = createApp(component)
+  app.mount("#user-info")
+})
